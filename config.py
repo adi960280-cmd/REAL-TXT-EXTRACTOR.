5ff22657ac0c9b464b911011b2b0b1b37e5f5d53
Tushar <tusartxt7570@gmail.com>
2025-05-14 15:33:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,5 +40,5 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002519097490"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mongodburl9721:rfNOuo4c1OSTZ7cs@cluster0.edezeww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002567278103"))
+FORCE_SUB = int(os.environ.get("FORCE_SUB", "-1002567278103"))
 
