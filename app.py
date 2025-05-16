ee4bf2c5689e24c2170b3bbd977c7b8768214f3e
Tushar <tusartxt7570@gmail.com>
2025-05-16 15:01:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Tech VJ'
+    return 'Hello from Tech TUSHAR'
 
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(host='0.0.0.0', port=8080)
+    
